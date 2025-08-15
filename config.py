c53275e3b01ae9e28e4a1fb0494313112f6eb91d
Abhinav3713 <diviteabhinav@gmail.com>
2025-08-15 12:31:18 +0530

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
-EMAIL_USER = "diviteabhinav0@gmail.com"
-EMAIL_PASSWORD = "ucny wrek woml xprq"
+EMAIL_USER = "your@gmail.com"
+EMAIL_PASSWORD = "Enter Your App Password"
 IMAP_SERVER = "imap.gmail.com"
 IMAP_PORT = 993
 FETCH_INTERVAL = 60  # Time in seconds (1 minute)
